50efe6e3b2fcd0b24128e61f75700f2ae8baefb9
James Fu <fu.jinghua8@gmail.com>
2026-03-12 14:55:33 -0700

    feat: experience on homepage from json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import experiences from '../data/experience.json';
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
 
         <p>
           consistent learner. here are some things i'm currently learning:
-          node.js, hip hop dance foundations, and how to put cook a mean steak.
+          node.js, hip hop dance foundations, and how to cook a mean steak.
           you'll probably catch me dancing, reading, cooking, or baking for
           friends!
         </p>
@@ -26,6 +27,17 @@ const Home = () => {
 
       <div>
         <h3>projects</h3>
+        {experiences.slice(0, 3).map((experience) => (
+          <div key={experience.company}>
+            <h3>{experience.company.toLowerCase()}</h3>
+            <p>
+              {experience.role.toLowerCase()} ({experience.start.toLowerCase()}{' '}
+              - {experience.end.toLowerCase()})
+            </p>
+            <p>{experience.summary.toLowerCase()}</p>
+            <p>technologies used: {experience.tech.join(', ').toLowerCase()}</p>
+          </div>
+        ))}
         <Link to="/projects">
           <p>all projects</p>
         </Link>
